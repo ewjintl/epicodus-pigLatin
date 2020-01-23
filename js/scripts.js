6a380c2fe810f65be793b84f21144ea1da6c05ee
elliot jordan and jonathan carlos <jonathan@>
2020-01-23 15:12:00 -0800

    stable first function

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,36 @@
-var translate = function() {
-  if (userSentence) {
-    userSentence.concat("ay");
-    alert(userSentence);
-  }
+// BACK-END LOGIC
+
+function vowelCheck(string) {
+  var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+  var splitSentence = string.split("");
+  console.log(splitSentence);
+  
+  if (vowels.includes(splitSentence[0])) {
+    var newSentence = splitSentence.concat("ay").join("");
+    console.log(newSentence);
+    }
+    return newSentence;
 }
 
-var vowels = ["a", "e", "i", "o", "u"]
-var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+
+function consonantCheck() {
+  var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"]
+  
+}
+
+function inputCheck() {
+  
+}
 
 
-
+// FRONT-END LOGIC
 $(document).ready(function() {
   $("form#translator").submit(function(event) {
     event.preventDefault();
     var userSentence = $("#userInput").val();
-    console.log(userSentence);
-
+    console.log(userSentence);            
     
-  });
-});
+    vowelCheck(userSentence);
+      });
+    });
+    
