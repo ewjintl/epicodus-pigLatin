731eed331783833113077c5913fdba31989dc74a
elliot jordan and jonathan carlos <jonathan@>
2020-01-23 16:15:34 -0800

    process words stating with one consonant -- next step will need looping to process an entir sentence instead of a single word.

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ function vowelCheck(string) {
   var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
   var splitSentence = string.split("");
   console.log(splitSentence);
-  
+
   if (vowels.includes(splitSentence[0])) {
     var newSentence = splitSentence.concat("ay").join("");
     console.log(newSentence);
@@ -13,9 +13,13 @@ function vowelCheck(string) {
 }
 
 
-function consonantCheck() {
-  var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"]
-  
+function consonantCheck(string) {
+  // var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"]
+  var splitSentence = string.split("");
+  var consoSentence = []
+  consoSentence = splitSentence.slice(1, splitSentence.length-1).concat(splitSentence[0], "ay").join("");
+  console.log(consoSentence);
+  return consoSentence;
 }
 
 function inputCheck() {
@@ -28,9 +32,15 @@ $(document).ready(function() {
   $("form#translator").submit(function(event) {
     event.preventDefault();
     var userSentence = $("#userInput").val();
-    console.log(userSentence);            
+    console.log(userSentence);
+    if (!userSentence) {
+      alert("Please enter a valid word or sentence.");
+    } else 
+
     
     vowelCheck(userSentence);
+    consonantCheck(userSentence);
+
       });
     });
     
